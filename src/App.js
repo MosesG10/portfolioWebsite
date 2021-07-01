ec33ebf8fcf27bfe3ea9b58e458f4ac51f311b4f
Moses Godinez <mosesgodinez@outlook.com>
2021-07-01 13:52:42 -0700

    style(Card): centering and aligning elements

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ const App = ()=> {
       <SubHeader/>
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-md-6 justify-content-center">
+          <div className="col-md-6">
             <Card image={schoolImage} subHeader="Industrial Engineering"/>
           </div>
-          <div className="col-md-6 justify-content-center">
+          <div className="col-md-6">
             <Card image={amexImage} subHeader="Software Engineer"/>
           </div>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import styles from '../styles.module.css';
+import PropTypes from 'prop-types';
 
+// eslint-disable-next-line react/prop-types
 const Card = ({image, subHeader}) => {
     return (
     <div className={styles.card}>
-        <img className={styles.cardArt} src={image} alt={"title"}></img>
-        <div>
+        <div className={styles.customContainer}>
+            <img className={styles.cardArt} src={image} alt={"title"}></img>
+        </div>
+        <div className={styles.customContainer}>
             <h4>{subHeader}</h4>
         </div>
     </div>
     );
+};
+
+Card.PropTypes = {
+    image: PropTypes.node.isRequired,
+    subHeader: PropTypes.string,
+};
+
+Card.defaultProps = {
+    subHeader: '',
 };
 
 export default Card;
